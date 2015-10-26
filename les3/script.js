384a9b60818a2be3919ad1952ef6d1a6760ef8cf
xIsabelG <isabel_love1998@hotmail.com>
2015-10-26 11:34:59 +0100

    ?????

---
+++ les3/script.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", function(){
+
+	var person = {
+	    firstName:"Idris",
+	    lastName:"Dopico",
+	    age:19,
+	    eyeColor:"brown",
+	    favouriteColour:"paars"
+	    getFirstName: function(){
+	    	return this.age;
+	    } 
+	};
+
+
+	clear = function(){
+		this.length = 0;
+
+	};
+	var age = [1,2,3,4,5];
+
+	console.log(age.length);
+	age.clear();
+	console.log(age.length);
+
+	createNewUser = function(firstName, lastName){
+		firstName = "Isabel",
+		lastName = "iets",
+		age = 17,
+		eyeColor = "groen?",
+		favouriteColour = "paars"		
+	
+		return {};
+	};
+
+	
+	debugger;
+});
+
